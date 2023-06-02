307d1dc6f0fa867faf14afaf24934addd0d77cd4
PepinQ <119268227+PepinQ@users.noreply.github.com>
2023-06-01 22:40:25 -0300

    Update Mesa.java

--- Mesa.java
+++ Mesa.java
@@ -1,9 +1,13 @@
 public class Mesa {
     public double preco;
     public String statusMesa = "Mesa Livre";
-    public String statusPedido;
-
-    public Mesa(String statusMesa){
+    public String statusPedido = "Não Realizado";
+    public String pedido;
+    public Mesa(String statusMesa, String statusPedido){
         this.statusMesa = statusMesa;
+        this.statusPedido = statusPedido;
+    }
+    public Mesa(){
+
     }
 }
